ee8942e9b668a0eb1e8f422db3b994483207f8f2
LeonGanz <100405315+LeonGantengz@users.noreply.github.com>
2022-04-04 04:06:11 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,7 +4,7 @@ let handler = async function (m, { conn }) {
     let name = DATABASE.data.users[i] ? DATABASE.data.users[i].name : conn.getName(i)
     list.push({
       "displayName": name,
-      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;Rama;;;\nFN:Rama\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Rama Agung Supriyadi\Web;Web: https://bl4ck-lion.github.io/index.phpnREGION; REGION : INDONESIA\nEND:VCARD`
+      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Rama Agung Supriyadi\Web;Web: https://bl4ck-lion.github.io/index.phpnREGION; REGION : INDONESIA\nEND:VCARD`
     })
   }
   await conn.sendMessage(m.chat, {
