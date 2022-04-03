d602fe083c6ac63577dc1a52bd12ae8d559f07ac
LeonGanz <100405315+LeonGantengz@users.noreply.github.com>
2022-04-04 04:11:01 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -7,7 +7,7 @@ let handler = async function (m, { conn }) {
       "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;${name};;;\nFN:${name}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Rama Agung Supriyadi\Web;Web: https://bl4ck-lion.github.io/index.phpnREGION; REGION : INDONESIA\nEND:VCARD`
     })
   }
-  await conn.sendMessage(m.chat, {
+  await this.sendMessage(m.chat, {
     "displayName": `${list.length} Contact`,
     "contacts": list
   }, 'contactsArrayMessage', { quoted: m })
