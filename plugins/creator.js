1af68e38211e361f11b383cfad70323fb15acb0f
LeonGanz <100405315+LeonGantengz@users.noreply.github.com>
2022-04-04 04:02:50 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,10 +1,19 @@
-function handler(m) {
-  const data = global.owner.filter(([id, isCreator]) => id && isCreator)
-  this.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
+let handler = async function (m, { conn }) {
+  let list = []
+  for (let i of creator.map(v => v + '@s.whatsapp.net')) {
+    let name = DATABASE.data.users[i] ? DATABASE.data.users[i].name : conn.getName(i)
+    list.push({
+      "displayName": name,
+      "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:;Rama;;;\nFN:Rama\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Rama Agung Supriyadi\Web;Web: https://bl4ck-lion.github.io/index.phpnREGION; REGION : INDONESIA\nEND:VCARD`
+    })
+  }
+  await conn.sendMessage(m.chat, {
+    "displayName": `${list.length} Contact`,
+    "contacts": list
+  }, 'contactsArrayMessage', { quoted: m })
 }
-handler.help = ['owner', 'creator']
+handler.help = ['owner', 'creator', 'own']
 handler.tags = ['info']
-
 handler.command = /^(owner|creator)$/i
 
-export default handler
+module.exports = handler
